5d577418ffd5f8d1947daef8c8e30d1f0764afb4
cityji <saurabh.paytmservices@gmail.com>
2021-07-25 22:45:18 +0530

    improved visuals

--- open.js
+++ open.js
@@ -4,12 +4,20 @@ let display = document.getElementById("screen");
 setInterval(() => {
     number = document.getElementById("numb").value;
 }, 500);
-
-function send() {
-    if (number=='') {
-        alert("Please Input The number First,")
-    } else {       
-        display.innerHTML = "Okay. Let me see..."
-        window.location.href="https://wa.me/91"+number+"?text=Hi"
+function chk() {
+    if (number.length == 9) {
+        display.innerHTML='<span id="allok"> Redy To Check </span>';
     }
+    else{
+        display.innerHTML='<span id="error">invalid number</span>';
+    }
+}
+function send() {
+    if (number == '') {
+        alert("Please Input The number First,")
+    } else {
+        display.innerHTML = "Okay. Let me see..."
+        window.location.href = "https://wa.me/91" + number + "?text=Hi"
+    }
+
 }
